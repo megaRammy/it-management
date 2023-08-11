6956958c6f2f6a360dc1529e1ac4b6c10ac9c1b1
megaRammy <github@rammy.moe>
2023-08-11 13:50:17 +0100

    Tinkering to get nag to work

--- ps-scripts/bentley-nag-suppress.ps1
+++ ps-scripts/bentley-nag-suppress.ps1
@@ -1,25 +1,27 @@
-# Elevate to Admin (UAC) (this is just grabbed from StackOverflow, can't tell you how it works. It works though! 😊)
+<## Elevate to Admin (UAC) (this is just grabbed from StackOverflow, can't tell you how it works. It works though! 😊)
 If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
 {
   # Relaunch as an elevated process:
   Start-Process powershell.exe "-File",('"{0}"' -f $MyInvocation.MyCommand.Path) -Verb RunAs
   exit
-}
+}#>
 
-$filename             = 'C:\Program Files\Common Files\Bentley Shared\CONNECTION Client\Bentley.Connect.Client.exe.config'
-$PopupIntervalMinutes = "1440"
+#$filename             = "C:\Program Files\Common Files\Bentley Shared\CONNECTION Client\Bentley.Connect.Client.exe.config"
+$filename = "C:\Users\sfielding\Bentley.Connect.Client.exe.config"
+$nodeName = '//appSettings/add[@key=PopupIntervalMinutes]'
+$value = "1440"
 
 # Load the xml document
 $xmlDoc = New-Object xml
-$xmlDoc.Load($filename)
 
-# Select all applicable nodes
-$nodes = $xmlDoc.SelectNodes('//appSettings/add[@key="PopupIntervalMinutes"]')
+$xmlDoc = [xml](Get-Content -Path "C:\Users\sfielding\Bentley.Connect.Client.exe.config")
 
-# In each node, set the value to the value of $PopupIntervalMinutes
-foreach($node in $nodes){
-    $PopupIntervalMinutes
+# Locate target node
+$targetNode = $xmlDoc.SelectNodes('//appSettings/add[@key=PopupIntervalMinutes]')
+
+# Overwrite attribute value
+foreach ($node in $targetNode) {
+  $node.value = ("1440")
 }
-
-# Save the document
-$xmlDoc.Save($filename)
+# save file
+$xmlDoc.Save("C:\Users\sfielding\Bentley.Connect.Client.exe.config")
